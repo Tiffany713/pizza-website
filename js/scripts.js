41880f2546273fc3acc6c18c73771a375cb74988
Tiffany Nguyen <tiffany.ngu713@gmail.com>
2020-01-31 10:08:11 -0800

    create topping and size objects and nested it within another object

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function Toppings()
+
+// List of Toppings
+var olives = {name: "Olives"};
+var spinach = {name: "Spinach"};
+var greenPepper = {name: "Green Pepper Bell"};
+var pepperoni = {name: "Pepperoni"};
+var mushrooms = {name: "Mushrooms"};
+var onions = {name: "Onions"};
+var pineapple = {name: "Pinapple"};
+var sausage = {name: "Sausage"};
+var extraCheese = {name: "Extra Cheese"};
+var jalapeno = {name: "Jalapeno"};
+
+// List of Sizes
+var small = {name: "Small"};
+var medium = {name: "Medium"};
+var large = {name: "Large"};
+var extraLarge = {name: "Extra Large"};
+
+var toppingsPizza = {name: "Toppings", toppings: [olives, spinach, greenPepper, pepperoni, mushrooms, onions, pineapple, sausage, extraCheese, jalapeno]};
+var sizePizza = {name: "Sizes", sizes: [small, medium, large, extraLarge]};
